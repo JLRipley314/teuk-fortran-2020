240c1108fca121f78b40329027023f2f2bba61f3
Justin L. Ripley <jripley@princeton.edu>
2020-07-02 21:23:32 -0400

    trying out complex numbers

--- main.f90
+++ main.f90
@@ -1,5 +1,11 @@
 program main
+    use iso_fortran_env, only: double=>real64
+
     implicit none
 
+    complex(double) :: a = (0,1)
+    complex(double) :: b = (0,0)
+
     print '(A)', 'starting'
+    print *, a*a 
 end program main
