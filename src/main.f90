ba476543b07ddd4ce2c853db7020b879e418eba9
Justin L. Ripley <jripley@princeton.edu>
2020-07-04 19:08:09 -0400

    working on constructors

--- src/main.f90
+++ src/main.f90
@@ -1,14 +1,11 @@
 program main
+!-----------------------------------------------------------------------------
     use, intrinsic :: iso_fortran_env, only: prec=>real64
 
-    use teuk
+    use mod_teuk
 
     implicit none
-
-    complex(prec) :: a = (0,1)
-    complex(prec) :: b = (2,1)
-
+!-----------------------------------------------------------------------------
     print '(A)', 'starting'
-    print *, a*a 
-    print *, b+a 
+!-----------------------------------------------------------------------------
 end program main
--- src/teuk.f90
+++ src/teuk.f90
@@ -1,33 +1,91 @@
-module teuk
-    use, intrinsic :: iso_fortran_env, only: rp=>real64, ip=>int64
-    use mod_field 
+module mod_teuk
+!-----------------------------------------------------------------------------
+    use, intrinsic :: iso_fortran_env, only: ip => int64, rp => real64
+
+    use mod_field
+
     implicit none
 !-----------------------------------------------------------------------------
-    real(rp), allocatable :: &
+    private
+    public :: Teuk 
+!-----------------------------------------------------------------------------
+    type :: Teuk
+    
+    integer(ip) :: dims(2)
+
+    complex(rp), allocatable :: &
         A_pp(:,:), A_pq(:,:), A_pf(:,:), &
         A_qp(:,:), A_qq(:,:), A_qf(:,:), &
         A_fp(:,:), A_fq(:,:), A_ff(:,:), &
         B_pp(:,:), B_pq(:,:), B_pf(:,:), &
         B_qp(:,:), B_qq(:,:), B_qf(:,:), &
         B_fp(:,:), B_fq(:,:), B_ff(:,:)
+
+    end type Teuk
 !-----------------------------------------------------------------------------
-    contains
+    interface Teuk 
+        module procedure :: teuk_constructor
+    end interface Teuk 
 !-----------------------------------------------------------------------------
-    subroutine init_teuk(nx,ny)
-        integer(ip), intent(in) :: nx, ny
+contains
+!-----------------------------------------------------------------------------
+    type(Teuk) function teuk_constructor(dims) result(self)
+        integer(ip), intent(in) :: dims(2) ! domain size in x and y
 
         integer(ip) :: i, j
 
-        j_loop: do j=1,ny
-            i_loop: do i=1,nx
+        allocate(self % A_pp(dims(1),dims(2)))
+        allocate(self % A_pq(dims(1),dims(2)))
+        allocate(self % A_pf(dims(1),dims(2)))
 
-                A_pp(i,j) = 1
-                A_pq(i,j) = 1
+        allocate(self % A_qp(dims(1),dims(2)))
+        allocate(self % A_qq(dims(1),dims(2)))
+        allocate(self % A_qf(dims(1),dims(2)))
 
-            end do i_loop
-        end do j_loop
+        allocate(self % A_fp(dims(1),dims(2)))
+        allocate(self % A_fq(dims(1),dims(2)))
+        allocate(self % A_ff(dims(1),dims(2)))
 
-    end subroutine init_teuk
+        allocate(self % B_pp(dims(1),dims(2)))
+        allocate(self % B_pq(dims(1),dims(2)))
+        allocate(self % B_pf(dims(1),dims(2)))
+
+        allocate(self % B_qp(dims(1),dims(2)))
+        allocate(self % B_qq(dims(1),dims(2)))
+        allocate(self % B_qf(dims(1),dims(2)))
+
+        allocate(self % B_fp(dims(1),dims(2)))
+        allocate(self % B_fq(dims(1),dims(2)))
+        allocate(self % B_ff(dims(1),dims(2)))
+
+        y_loop: do j=1,dims(2)
+            x_loop: do i=1,dims(1)
+                self % A_pp(i,j) = (0,0)
+                self % A_pq(i,j) = (0,0)
+                self % A_pf(i,j) = (0,0)
+
+                self % A_qp(i,j) = (0,0)
+                self % A_qq(i,j) = (0,0)
+                self % A_qf(i,j) = (0,0)
+
+                self % A_fp(i,j) = (0,0)
+                self % A_fq(i,j) = (0,0)
+                self % A_ff(i,j) = (0,0)
+
+                self % B_pp(i,j) = (0,0)
+                self % B_pq(i,j) = (0,0)
+                self % B_pf(i,j) = (0,0)
+
+                self % B_qp(i,j) = (0,0)
+                self % B_qq(i,j) = (0,0)
+                self % B_qf(i,j) = (0,0)
+
+                self % B_fp(i,j) = (0,0)
+                self % B_fq(i,j) = (0,0)
+                self % B_ff(i,j) = (0,0)
+            end do x_loop
+        end do y_loop
+
+    end function teuk_constructor
 !-----------------------------------------------------------------------------
-end module teuk
-
+end module mod_teuk
