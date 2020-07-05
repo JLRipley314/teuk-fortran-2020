b0ab112007be9362e9583a855b801dfe081eec69
Justin L. Ripley <jripley@princeton.edu>
2020-07-05 18:03:09 -0400

    now compiles

--- src/bkgrd.f90
+++ src/bkgrd.f90
@@ -4,6 +4,7 @@ module mod_bkgrd
 
   use mod_sim_params, only: &
     nx, ny, cl, &
+    bhs, bhm, &
     r_pts, y_pts, cs, sn
 
   implicit none
@@ -21,7 +22,6 @@ contains
   subroutine init_bkgrd()
 
     integer(ip) :: i, j
-    complex(rp) :: val
 
     allocate(mu_0(nx,ny))
     allocate(ta_0(nx,ny))
@@ -35,12 +35,14 @@ contains
     y_loop: do j=1,ny
       x_loop: do i=1,nx
 
-        val = (2*cl,cl)
-
-        mu_0(i,j) = (cl,cl) !( &
-!        - ((cl**2) / ((cl**4) + (bhs**2)*(cs(j)**2)*(R(i)**2))), &
-!        - ((bhs*cs(j)*R(i)) / ((cl**4) + (bhs**2)*(cs(j)**2)*(R(i)**2))) & 
-!        )
+        mu_0(i,j) = cmplx( &
+        - ((cl**2) &
+          / ((cl**4) + (bhs**2)*(cs(j)**2)*(r_pts(i)**2)) &
+          ), &
+        - ((bhs*cs(j)*r_pts(i)) & 
+          / ((cl**4) + (bhs**2)*(cs(j)**2)*(r_pts(i)**2)) &
+          ), & 
+          kind=rp)
         ta_0(i,j) = 0
         pi_0(i,j) = 0
         rh_0(i,j) = 0
