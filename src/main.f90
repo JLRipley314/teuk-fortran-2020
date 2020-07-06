9fe30a9069d8c37657180dfb33caa8f83f0687a6
Justin L. Ripley <jripley@princeton.edu>
2020-07-06 11:05:17 -0400

    need to work in input of variables

--- src/main.f90
+++ src/main.f90
@@ -1,6 +1,6 @@
 program main
 !-----------------------------------------------------------------------------
-  use, intrinsic :: iso_fortran_env, only: prec=>real64
+  use, intrinsic :: iso_fortran_env, only: ip=>int64, rp=>real64
 
   use mod_field
   use mod_sim_params
@@ -9,8 +9,10 @@ program main
 
   implicit none
 !-----------------------------------------------------------------------------
-  print '(A)', 'starting'
+!-----------------------------------------------------------------------------
+  print '(a)', 'setting up fields'
 
   call init_bkgrd()
+
 !-----------------------------------------------------------------------------
 end program main
