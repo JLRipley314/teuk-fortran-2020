b80c2f4fed17452e4dd2ad9a9e0717c500421f43
Justin <lloydripley@gmail.com>
2020-07-16 14:07:38 -0700

    need to save real and imaginary parts seperately

--- src/mod_io.f90
+++ src/mod_io.f90
@@ -94,31 +94,59 @@ contains
    subroutine write_field_csv(f)
       type(field),  intent(in) :: f
 
-      character(:), allocatable :: rn
+      character(:), allocatable :: fn_re, fn_im
       logical :: exists
       integer(ip) :: i, j, ierror = 0
       integer(ip) :: uf = 3
       ! set the file name to read from
-      rn = output_dir // '/' // f%name // '.csv'
-
-      inquire(file=rn,exist=exists)
+      fn_re = output_dir // '/' // f%name // '_re.csv'
+      fn_im = output_dir // '/' // f%name // '_im.csv'
+      !----------------------------------------------------------------------
+      ! save real part
+      !----------------------------------------------------------------------
+      inquire(file=fn_re,exist=exists)
       if (exists) then
-         open(unit=uf,file=rn,status='old',position='append',action='write',iostat=ierror)
+         open(unit=uf,file=fn_re,status='old',position='append',action='write',iostat=ierror)
       else
-         open(unit=uf,file=rn,status='new',action='write',iostat=ierror) 
+         open(unit=uf,file=fn_re,status='new',action='write',iostat=ierror) 
       end if
-         do i=1,nx
-            do j=1,ny
-               write (uf,'(e12.6,a1)',advance='no',iostat=ierror) f%np1(i,j), ','
-            end do
-            ! line break
-            write (uf,*) 
+
+      do i=1,nx
+         do j=1,ny
+            write (uf,'(e12.6,a1)',advance='no',iostat=ierror) real(f%np1(i,j)), ','
          end do
+         ! line break
+         write (uf,*) 
+      end do
       close(uf)
 
       if (ierror/=0) then
          write (*,*) "Error(read_arr): ierror=", ierror
-         write (*,*) "file = ", rn
+         write (*,*) "file = ",fn_re
+         stop
+      end if
+      !----------------------------------------------------------------------
+      ! save imaginary part 
+      !----------------------------------------------------------------------
+      inquire(file=fn_im,exist=exists)
+      if (exists) then
+         open(unit=uf,file=fn_im,status='old',position='append',action='write',iostat=ierror)
+      else
+         open(unit=uf,file=fn_im,status='new',action='write',iostat=ierror) 
+      end if
+
+      do i=1,nx
+         do j=1,ny
+            write (uf,'(e12.6,a1)',advance='no',iostat=ierror) aimag(f%np1(i,j)), ','
+         end do
+         ! line break
+         write (uf,*) 
+      end do
+      close(uf)
+
+      if (ierror/=0) then
+         write (*,*) "Error(read_arr): ierror=", ierror
+         write (*,*) "file = ", fn_im
          stop
       end if
    end subroutine write_field_csv
@@ -128,18 +156,21 @@ contains
       character(*), intent(in) :: fn
       complex(rp),  dimension(nx,ny), intent(in) :: arr
 
-      character(:), allocatable :: rn
+      character(:), allocatable :: rn_re, rn_im
       logical :: exists
       integer(ip) :: i, j, ierror = 0
       integer(ip) :: uf = 3
       ! set the file name to read from
-      rn = output_dir // '/' // fn // '.csv'
-
-      inquire(file=rn,exist=exists)
+      rn_re = output_dir // '/' // fn // '_re.csv'
+      rn_im = output_dir // '/' // fn // '_im.csv'
+      !----------------------------------------------------------------------
+      ! save real part 
+      !----------------------------------------------------------------------
+      inquire(file=rn_re,exist=exists)
       if (exists) then
-         open(unit=uf,file=rn,status='old',position='append',action='write',iostat=ierror)
+         open(unit=uf,file=rn_re,status='old',position='append',action='write',iostat=ierror)
       else
-         open(unit=uf,file=rn,status='new',action='write',iostat=ierror) 
+         open(unit=uf,file=rn_re,status='new',action='write',iostat=ierror) 
       end if
          do i=1,nx
             do j=1,ny
@@ -151,7 +182,29 @@ contains
 
       if (ierror/=0) then
          write (*,*) "Error(read_arr): ierror=", ierror
-         write (*,*) "file = ", rn
+         write (*,*) "file = ", rn_re
+         stop
+      end if
+      !----------------------------------------------------------------------
+      ! save real part 
+      !----------------------------------------------------------------------
+      inquire(file=rn_im,exist=exists)
+      if (exists) then
+         open(unit=uf,file=rn_im,status='old',position='append',action='write',iostat=ierror)
+      else
+         open(unit=uf,file=rn_im,status='new',action='write',iostat=ierror) 
+      end if
+         do i=1,nx
+            do j=1,ny
+               write (uf,'(e12.6,a1)',advance='no',iostat=ierror) arr(i,j), ','
+            end do
+            write (uf,*) ! line break
+         end do
+      close(uf)
+
+      if (ierror/=0) then
+         write (*,*) "Error(read_arr): ierror=", ierror
+         write (*,*) "file = ", rn_im
          stop
       end if
    end subroutine write_array_csv
